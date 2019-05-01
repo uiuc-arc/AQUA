86dc04b49985edd78b5ba05f3e45799384b40097
Saikat Dutta <saikatdutta.pro2011@gmail.com>
2019-05-01 14:10:34 -0500

    new grammar

---
+++ src/main/java/AST.java
@@ -0,0 +1,379 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class AST {
+    public static class ASTNode{
+
+    }
+    public static class Program extends ASTNode{
+        protected ArrayList<Data> data;
+        protected ArrayList<Query> queries;
+        protected ArrayList<Statement> statements;
+        public Program(){
+            this.data = new ArrayList<>();
+            this.queries = new ArrayList<>();
+            this.statements = new ArrayList<>();
+        }
+
+        public Program(ArrayList<Data> data, ArrayList<Statement> statements,  ArrayList<Query> queries){
+            this.data = data;
+            this.statements = statements;
+            this.queries = queries;
+        }
+
+        public void addQuery(Query query){
+            this.queries.add(query);
+        }
+
+        public void addStatement(Statement statement){
+            this.statements.add(statement);
+        }
+
+        public void addData(Data data){
+            this.data.add(data);
+        }
+    }
+
+
+    public enum QueryType
+    {   POSTERIOR,
+        EXPECTATION }
+
+    public static class Query extends ASTNode{
+        public QueryType queryType;
+        public String id;
+    }
+
+    public static class Statement extends ASTNode{
+        public Annotation annotation;
+    }
+
+    public static class AssignmentStatement extends Statement{
+        public Expression lhs;
+        public Expression rhs;
+        public AssignmentStatement(Expression lhs, Expression rhs){
+            this.lhs = lhs;
+            this.rhs = rhs;
+        }
+
+    }
+
+    public static class ForLoop extends Statement{
+        public Id loopVar;
+        public Range range;
+        public Block block;
+        public ForLoop(Id loopVar, Range range, Block block){
+            this.loopVar = loopVar;
+            this.range = range;
+            this.block = block;
+        }
+
+    }
+
+    public static class Range extends ASTNode{
+        public Expression start;
+        public Expression end;
+        public Range(Expression start, Expression end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static class IfStmt extends Statement{
+        public Expression condition;
+        public Block trueBlock;
+        public Block elseBlock;
+        public IfStmt(){
+
+        }
+
+        public IfStmt(Expression condition, Block trueBlock, Block elseBlock){
+            this.condition = condition;
+            this.trueBlock = trueBlock;
+            this.elseBlock = elseBlock;
+        }
+    }
+
+    public static class TernaryIf extends  Expression{
+        public Expression condition;
+        public Expression trueExpression;
+        public Expression falseExpression;
+
+        public TernaryIf(Expression condition, Expression trueExpression, Expression falseExpression){
+            this.condition = condition;
+            this.trueExpression = trueExpression;
+            this.falseExpression = falseExpression;
+        }
+    }
+
+    public static class ExponOp extends Expression{
+        public Expression base;
+        public Expression power;
+        public ExponOp(Expression base, Expression power){
+            this.base = base;
+            this.power = power;
+        }
+    }
+
+    public static class AddOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public AddOp(Expression base, Expression power){
+            this.op1 = base;
+            this.op2 = power;
+        }
+    }
+
+    public static class MinusOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public MinusOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class MulOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public MulOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class DivOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public DivOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class LtOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public LtOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class LeqOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public LeqOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class GtOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public GtOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class GeqOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public GeqOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class NeOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public NeOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class EqOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public EqOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class AndOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public AndOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+    public static class OrOp extends Expression{
+        public Expression op1;
+        public Expression op2;
+        public OrOp(Expression op1, Expression op2){
+            this.op1 = op1;
+            this.op2 = op2;
+        }
+    }
+
+
+    public static class Block extends ASTNode{
+        ArrayList<Statement> statements;
+        public Block(){
+            this.statements = new ArrayList<>();
+        }
+    }
+
+    public static class Decl extends Statement{
+        public Dtype dtype;
+        public Dims dims;
+        public Id id;
+    }
+
+    public static class FunctionCallStatement extends Statement{
+        private final FunctionCall functionCall;
+        public FunctionCallStatement(FunctionCall functionCall){
+            this.functionCall = functionCall;
+        }
+    }
+
+    public static class FunctionCall extends Expression{
+        public boolean isDist;
+        public boolean isDistX;
+        public ArrayList<Expression> parameters;
+        public Id id;
+        public FunctionCall(){
+            this.isDist = false;
+            this.isDistX = false;
+            parameters = new ArrayList<>();
+        }
+
+    }
+
+    public enum Primitive {INTEGER, FLOAT}
+
+    public static class Expression extends ASTNode{
+
+    }
+
+    public static class ArrayAccess extends Expression{
+        public Id id;
+        public Dims dims;
+        public ArrayAccess(Id id, Dims dims){
+            this.id = id;
+            this.dims = dims;
+        }
+    }
+
+    public static class UnaryExpression extends Expression{
+        public Expression expression;
+        public UnaryExpression(Expression expression){
+            this.expression = expression;
+        }
+    }
+
+    public static class Transpose extends Expression{
+        public Expression expression;
+        public Transpose(Expression expression){
+            this.expression = expression;
+        }
+    }
+
+    public static class Dims extends ASTNode{
+        public ArrayList<Expression> dims = new ArrayList<>();
+    }
+
+    public static class Dtype extends ASTNode{
+        public Primitive primitive;
+        public Dims dims;
+    }
+
+
+    public static class Number extends Expression{
+
+    }
+
+    public static class Integer extends Number{
+        public int value;
+        public Integer(String integerValue){
+            this.value = java.lang.Integer.parseInt(integerValue);
+        }
+    }
+
+    public static class Double extends Number{
+        public double value;
+        public Double(String doubleValue){
+            this.value = java.lang.Double.parseDouble(doubleValue);
+        }
+    }
+
+    public static class Id extends Expression{
+        public String id;
+        public Id(String id){
+            this.id = id;
+        }
+    }
+
+    public static class StringValue extends Expression{
+        public String string;
+        public StringValue(String string){
+            this.string = string;
+        }
+    }
+
+    public static class Limits extends ASTNode{
+        public Expression lower;
+        public Expression upper;
+    }
+
+    public enum Marker{Start, End};
+
+    public static class MarkerWrapper extends ASTNode{
+        public Marker marker;
+        public MarkerWrapper(Marker marker){
+            this.marker = marker;
+        }
+    }
+
+    public static class Data extends ASTNode{
+        public Id id;
+        public Dtype dtype;
+        public Expression expression;
+        public Array array;
+        public Vector vector;
+    }
+
+    public static class ConstHole extends Expression{
+
+    }
+
+    public static class Array extends ASTNode{
+        public ArrayList<Expression> expressions;
+        public ArrayList<Array> arrays;
+        public ArrayList<Vector> vectors;
+    }
+
+    public static class Vector extends ASTNode{
+        public ArrayList<Expression> expressions;
+        public ArrayList<Array> arrays;
+        public ArrayList<Vector> vectors;
+    }
+
+    public enum AnnotationType {Limits, Blk, Type, Observe, Prior, Dimension }
+    public static class Annotation extends ASTNode{
+        public AnnotationType annotationType;
+        public ASTNode annotationValue;
+
+    }
+
+
+}
--- template/Template3.g4
+++ template/Template3.g4
@@ -1,5 +1,184 @@
 grammar Template3;
 
+primitive returns [AST.Primitive value]
+:
+INTEGERTYPE {$value=AST.Primitive.INTEGER;}
+| FLOATTYPE {$value=AST.Primitive.FLOAT;};
+
+
+number returns [AST.Number value]
+: i=INT {$value = new AST.Integer($i.getText());}
+| d=DOUBLE {$value = new AST.Double($d.getText());}
+;
+
+limits returns [AST.Limits value]
+@init {$value = new AST.Limits();}
+ : '<' ( 'lower' '=' expr {$value.lower=$expr.value;}',' 'upper' '=' expr {$value.upper=$expr.value;}
+ | 'lower' '=' expr {$value.lower=$expr.value;}
+ | 'upper' '=' expr {$value.upper=$expr.value;} ) '>' ;
+
+marker returns [AST.Marker value]
+: 'start' {$value=AST.Marker.Start;}
+| 'end' {$value=AST.Marker.End;};
+
+
+annotation_type returns [AST.AnnotationType value]
+: 'blk' {$value = AST.AnnotationType.Blk;}
+| 'type' {$value = AST.AnnotationType.Type;}
+| 'dimension' {$value = AST.AnnotationType.Dimension;}
+| 'limits' {$value = AST.AnnotationType.Limits;}
+| 'observe' {$value = AST.AnnotationType.Observe;}
+| 'prior' {$value = AST.AnnotationType.Prior;}
+;
+
+annotation_value returns [AST.ASTNode value]
+: ID {$value = new AST.Id($ID.getText());}
+| dims {$value = $dims.value;}
+| limits {$value = $limits.value;}
+| marker {$value = new AST.MarkerWrapper($marker.value);}
+;
+
+// annotation for block, types, prior and observes
+annotation returns [AST.Annotation value]
+@init {$value = new AST.Annotation();}
+: '@' at=annotation_type {$value.annotationType = $at.value;}
+( av=annotation_value {$value.annotationValue = $av.value;})?;
+
+
+//// types and dimensions
+dims returns [AST.Dims value]
+@init {$value = new AST.Dims();}
+: e1=expr {$value.dims.add($e1.value);} (',' e2=expr {$value.dims.add($e2.value);})*;
+//vectorDIMS : '<' dims '>';
+
+dtype returns [AST.Dtype value]
+@init {$value = new AST.Dtype();}
+ : p=primitive {$value.primitive = $p.value;} '[' dims {$value.dims=$dims.value;} ']'? ;
+//
+//// data
+array returns [AST.Array value]
+@init {$value = new AST.Array();}
+ :
+ '[' expr {$value.expressions = new ArrayList<>(); $value.expressions.add($expr.value);} ( ',' expr {$value.expressions.add($expr.value);})* ']'
+ | '[' array {$value.arrays = new ArrayList<>(); $value.arrays.add($array.value);}(',' array {$value.arrays.add($array.value);})* ']'
+ | '[' vector {$value.vectors = new ArrayList<>(); $value.vectors.add($vector.value);} (',' vector {$value.vectors.add($vector.value);})* ']'
+ | '[' ']';
+vector returns [AST.Vector value]
+
+ @init {$value = new AST.Vector();}
+  :
+  '<' expr {$value.expressions = new ArrayList<>(); $value.expressions.add($expr.value);} ( ',' expr {$value.expressions.add($expr.value);})* '>'
+  |  '<' array {$value.arrays = new ArrayList<>(); $value.arrays.add($array.value);} (',' array {$value.arrays.add($array.value);})* '>'
+  | '<' vector {$value.vectors = new ArrayList<>(); $value.vectors.add($vector.value);} (',' vector {$value.vectors.add($vector.value);})* '>'
+  | '<' '>';
+
+//
+//// statements
+data returns [AST.Data value]
+@init {$value = new AST.Data();}
+: ID {$value.id = new AST.Id($ID.getText());} ':'
+( dtype {$value.dtype = $dtype.value;}
+| expr  {$value.expression = $expr.value;}
+| array {$value.array = $array.value;}
+| vector {$value.vector = $vector.value;}
+) ;
+
+function_call returns [AST.FunctionCall value]
+@init {$value = new AST.FunctionCall();}
+: (( ID {$value.id = new AST.Id($ID.getText());} | DISTHOLE {$value.isDist = true;} ) '(' (e1=expr {$value.parameters.add($e1.value);}  (',' e2=expr {$value.parameters.add($e2.value);} )*)? ')' )
+| DISTXHOLE {$value.isDistX = true;}
+;
+for_loop returns [AST.ForLoop value]
+: 'for' '(' ID 'in' e1=expr ':' e2=expr ')' block {new AST.ForLoop(new AST.Id($ID.getText()), new AST.Range($e1.value, $e2.value), $block.value);};
+
+if_stmt returns [AST.IfStmt value]
+@init {$value = new AST.IfStmt();}
+: 'if' '(' e=expr {$value.condition = $e.value;} ')' b1=block {$value.trueBlock=$b1.value;} ('else' b2=block {$value.elseBlock = $b2.value;})? ;
+
+assign returns [AST.AssignmentStatement value]
+: e1=expr '=' e2=expr {$value = new AST.AssignmentStatement($e1.value, $e2.value); }
+;
+
+decl returns [AST.Decl value]
+@init {$value = new AST.Decl();}
+: dtype {$value.dtype=$dtype.value;} ID {$value.id = new AST.Id($ID.getText());} ('[' dims {$value.dims=$dims.value;} ']')?
+//| function_decl
+;
+
+
+statement returns [AST.Statement value]
+@init {AST.Annotation at = null;}
+: (annotation {at = $annotation.value;})?
+  assign  {$value = $assign.value;}
+| if_stmt {$value = $if_stmt.value;}
+| for_loop {$value = $for_loop.value;}
+| function_call {$value = new AST.FunctionCallStatement($function_call.value); }
+| decl {$value= $decl.value;}
+{$value.annotation = at;}
+;
+
+block returns [AST.Block value]
+@init {$value=new AST.Block();}
+: '{' (s=statement {$value.statements.add($s.value);})* '}'
+| s=statement {$value.statements.add($s.value);};
+//
+//// function definitions
+//fparam : return_or_param_type ID;
+//fparams : fparam (',' fparam)*;
+//
+//return_or_param_type: dtype | 'void' | ( dtype ('[' ']')+ ) ;
+//function_decl: return_or_param_type ID '(' fparams? ')' block;
+//
+expr returns [AST.Expression value]
+:
+    expr '\''         {$value = new AST.Transpose($expr.value);}
+    | e1=expr '?' e2=expr ':' e3=expr {$value = new AST.TernaryIf($e1.value, $e2.value, $e3.value);}
+
+    | e1=expr '^' e2=expr     {$value = new AST.ExponOp($e1.value, $e2.value);}
+    | e1=expr '/' e2=expr     {$value = new AST.DivOp($e1.value, $e2.value);}
+    | e1=expr '*' e2=expr     {$value = new AST.MulOp($e1.value, $e2.value);}
+    | e1=expr '+'  e2=expr    {$value = new AST.AddOp($e1.value, $e2.value);}
+    | e1=expr '-' e2=expr    {$value = new AST.MinusOp($e1.value, $e2.value);}
+//    | expr './' expr    #vecdivop
+//    | expr '.*' expr    #vecmulop
+    | e1=expr '<' e2=expr     {$value = new AST.LtOp($e1.value, $e2.value);}
+    | e1=expr '<=' e2=expr    {$value = new AST.LeqOp($e1.value, $e2.value);}
+    | e1=expr '>' e2=expr     {$value = new AST.GtOp($e1.value, $e2.value);}
+    | e1=expr '>=' e2=expr    {$value = new AST.GeqOp($e1.value, $e2.value);}
+    | e1=expr '!=' e2=expr    {$value = new AST.NeOp($e1.value, $e2.value);}
+    | e1=expr '==' e2=expr    {$value = new AST.EqOp($e1.value, $e2.value);}
+    | e1=expr '&&' e2=expr    {$value = new AST.AndOp($e1.value, $e2.value);}
+    | e1=expr '||' e2=expr    {$value = new AST.OrOp($e1.value, $e2.value);}
+//    | expr '[' expr ':' expr ']' #subset
+    | function_call         {$value = $function_call.value;}
+    | '-' expr              {$value = new AST.UnaryExpression($expr.value);}
+    | '(' expr ')'          {$value = $expr.value;}
+    | n=number              {$value = $n.value;}
+    | id=ID                 {$value = new AST.Id($id.getText()); }
+    | ID '[' dims ']'       {$value = new AST.ArrayAccess(new AST.Id($ID.getText()), $dims.value);}
+    | st=STRING             {$value = new AST.StringValue($st.getText());}
+    | CONSTHOLE             {$value = new AST.ConstHole();}
+    ;
+
+query returns  [AST.Query value]
+@init {$value = new AST.Query();}
+: ('posterior'  {$value.queryType = AST.QueryType.POSTERIOR;}
+| 'expectation' {$value.queryType = AST.QueryType.EXPECTATION;})
+'(' d=ID ')'      {$value.id = $d.getText();}
+;
+
+template returns [AST.Program value]
+@init{ $value = new AST.Program(); }
+:
+(
+  d=data          {$value.addData($d.value);}
+| st=statement    {$value.addStatement($st.value);}
+| q=query         {$value.addQuery($q.value);}
+)* ;
+
+
+// lexer rules
+
 WS : [ \n\t\r]+ -> channel(HIDDEN) ;
 
 // primitives
@@ -13,72 +192,3 @@ FLOATTYPE: 'float';
 DISTHOLE: 'DIST';
 CONSTHOLE: 'CONST';
 DISTXHOLE: 'DISTX';
-
-primitive: INTEGERTYPE | FLOATTYPE;
-number: INT | DOUBLE;
-limits : '<' ( 'lower' '=' expr ',' 'upper' '=' expr | 'lower' '=' expr | 'upper' '=' expr ) '>' ;
-marker : 'start' | 'end';
-//dotted_name: ID ('.' ID)*;
-annotation_type: 'blk' | 'type' | 'dimension' | 'limits' | 'observe' | 'prior';
-annotation_value: ID | dims | limits | marker;
-annotation: '@' annotation_type ('.' annotation_value)?; // annotation for block, types, prior and observes
-
-// types and dimensions
-dims: expr (',' expr)*;
-vectorDIMS : '<' dims '>';
-dtype : primitive '[' dims ']'?;
-
-// data
-array : '[' expr ( ',' expr )* ']' | '[' array (',' array)* ']' | '[' vector (',' vector)* ']' | '[' ']';
-vector : '<' expr ( ',' expr )* '>' | '<' vector (',' vector)* '>' | '<' array (',' array)* '>' | '<' '>';
-
-// statements
-data: ID ':'  ( dtype | expr | array | vector ) ;
-//prior: expr ':=' expr  ;
-function_call : (( ID | DISTHOLE ) '(' (expr (',' expr)*)? ')' ) | DISTXHOLE;
-for_loop: 'for' '(' ID 'in' expr ':' expr ')' block;
-if_stmt: 'if' '(' expr ')' block ('else' 'if' '(' expr ')' block )* ('else' block)? ;
-assign: expr '=' expr ;
-decl: dtype ('[' dims ']')? ID ('[' dims ']')? | function_decl;
-
-statement: assign | for_loop | if_stmt | decl | function_call | annotation;
-block: '{' statement* '}' | statement ;
-
-// function definitions
-fparam : return_or_param_type ID;
-fparams : fparam (',' fparam)*;
-
-return_or_param_type: dtype | 'void' | ( dtype ('[' ']')+ ) ;
-function_decl: return_or_param_type ID '(' fparams? ')' block;
-
-expr :
-      expr '\''         #transpose
-    | expr '?' expr ':' expr #ternary
-    // | expr AOP expr     #arith
-    | expr '^' expr     #exponop
-    | expr '/' expr     #divop
-    | expr '*' expr     #mulop
-    | expr '+'  expr    #addop
-    | expr '-' expr     #minusop
-    | expr './' expr    #vecdivop
-    | expr '.*' expr    #vecmulop
-    | expr '<' expr     #lt
-    | expr '<=' expr    #leq
-    | expr '>' expr     #gt
-    | expr '>=' expr    #geq
-    | expr '!=' expr    #ne
-    | expr '==' expr    #eq
-    | expr '&&' expr    #and
-    | expr '[' expr ':' expr ']' #subset
-    | function_call     #function
-    | '-' expr          #unary
-    | '(' expr ')'      #brackets
-    | number            #val
-    | ID                #ref
-    | ID '[' dims ']'   #array_access
-    | STRING            #string
-    | CONSTHOLE         #consthole
-    ;
-
-query: ('posterior' | 'expectation') '(' ID ')' ;
-template: (data | statement | query)* ;
