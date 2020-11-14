abe1479397c56c16c8c75af43031b0ca52cd1700
zixinh2 <zixinh2@illinois.edu>
2020-11-14 02:31:48 -0600

    new

--- src/test/java/tests/TestIntervalAnalysis.java
+++ src/test/java/tests/TestIntervalAnalysis.java
@@ -30,7 +30,7 @@ public class TestIntervalAnalysis {
     public void Test5() throws IOException {
 
         String localDir = "/Users/zixin/Documents/uiuc/fall20/analysis/progs/";
-        AnalysisRunner.analyzeProgram(localDir, "unemployment");
+        AnalysisRunner.analyzeProgram(localDir, "y_x");
 
     }
 
